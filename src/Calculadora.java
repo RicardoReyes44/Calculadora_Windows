d5da0b17ffc77ef9c3bbddc8c019427d6594b248
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-11 11:24:23 -0600

    Se establecio el tamaño, boton de cerrado, visibilidad y layout de la clase 'Calculadora'

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,21 @@
+import java.awt.*;
 import javax.swing.JFrame;
 
 
 public class Calculadora extends JFrame{
 
-	
+	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
+
+	public Calculadora(){
+		getContentPane().setLayout(gbl);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Calculadora");
+		setVisible(true);
+		
+		pack();
+		setLocationRelativeTo(null);
+		
+	}
 
 }
