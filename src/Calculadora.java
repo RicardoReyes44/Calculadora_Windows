310a5eff4ab16c90b28f9c03d6bf7bb3c812f56e
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-11 16:37:43 -0600

    Se agregaron las configuraciones de 'x', 'y', 'gridheight' y 'gridwidth' en la clase 'Calculadora'

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,33 +4,33 @@ import javax.swing.*;
 
 public class Calculadora extends JFrame{
 
-	JButton btnSuma = new JButton();
-	JButton btnResta = new JButton();
-	JButton btnMult = new JButton();
-	JButton btnDiv = new JButton();
-	JButton btnUnoSobreX = new JButton();
-	JButton btnIgual = new JButton();
+	JButton btnSuma = new JButton("+");
+	JButton btnResta = new JButton("-");
+	JButton btnMult = new JButton("x");
+	JButton btnDiv = new JButton("/");
+	JButton btnUnoSobreX = new JButton("1/x");
+	JButton btnIgual = new JButton("=");
 
-	JButton btnBorrar = new JButton();
-	JButton btnCuadrado = new JButton();
-	JButton btnNueve = new JButton();
-	JButton btnSeis = new JButton();
-	JButton btnTres = new JButton();
-	JButton btnPunto= new JButton();
+	JButton btnBorrar = new JButton("<-");
+	JButton btnCuadrado = new JButton("x²");
+	JButton btnNueve = new JButton("9");
+	JButton btnSeis = new JButton("6");
+	JButton btnTres = new JButton("3");
+	JButton btnPunto= new JButton(".");
 
-	JButton btnUno = new JButton();
-	JButton btnCuatro = new JButton();
-	JButton btnSiete = new JButton();
-	JButton btnCE = new JButton();
-	JButton btnPorcentaje = new JButton();
-	JButton btnMasMenos = new JButton();
+	JButton btnUno = new JButton("1");
+	JButton btnCuatro = new JButton("4");
+	JButton btnSiete = new JButton("5");
+	JButton btnCE = new JButton("CE");
+	JButton btnPorcentaje = new JButton("%");
+	JButton btnMasMenos = new JButton("+-");
 
-	JButton btnOcho = new JButton();
-	JButton btnC = new JButton();
-	JButton btnRaiz = new JButton();
-	JButton btnCinco = new JButton();
-	JButton btnDos = new JButton();
-	JButton btnCero = new JButton();
+	JButton btnOcho = new JButton("8");
+	JButton btnC = new JButton("C");
+	JButton btnRaiz = new JButton("√");
+	JButton btnCinco = new JButton("5");
+	JButton btnDos = new JButton("2");
+	JButton btnCero = new JButton("0");
 	
 	GridBagLayout gbl = new GridBagLayout();
 	GridBagConstraints gbc = new GridBagConstraints();
@@ -40,10 +40,12 @@ public class Calculadora extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Calculadora");
 		setVisible(true);
-		
+
 		// --------------------------------------------------
 		
-        
+        alinearElemento(0, 0, 80, 100, btnSuma);
+        alinearElemento(1, 0, 0, 100, new Label(""));
+        alinearElemento(2, 0, 80, 100, btnResta);
 		
 		// --------------------------------------------------
 		
@@ -53,6 +55,14 @@ public class Calculadora extends JFrame{
 	}
 	
 	public void alinearElemento(int x, int y, int height, int width, Component componente) {
+		
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridheight = height;
+		gbc.gridheight = height;
+		
+		gbl.setConstraints(componente, gbc);
+		add(componente);
 		
 	}
 
